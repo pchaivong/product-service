6d31a0543c1f5762bd4f8950b82aafc42e9402d4
Prawit Chaivong <prawit.c@zeabix.com>
2024-03-27 02:18:16 +0700

    Add more unittest

--- internal/core/service/service_test.go
+++ internal/core/service/service_test.go
@@ -3,6 +3,7 @@ package service_test
 import (
 	"testing"
 
+	"github.com/pchaivong/product-service/internal/core/domain"
 	"github.com/pchaivong/product-service/internal/core/service"
 	"github.com/pchaivong/product-service/mock"
 )
@@ -10,4 +11,41 @@ import (
 func TestNewProductService(t *testing.T) {
 	m := mock.NewMockRepository()
 	service.NewProductService(m)
+}
+
+func TestCreateProduct(t *testing.T) {
+	m := mock.NewMockRepository()
+	s := service.NewProductService(m)
+
+	_, err := s.CreateProduct("mock", "MOCK", "MOCK", 0.0, true)
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func TestListProduct(t *testing.T) {
+	m := mock.NewMockRepository()
+	s := service.NewProductService(m)
+
+	s.ListProduct()
+}
+
+func TestUpdateProduct(t *testing.T) {
+	m := mock.NewMockRepository()
+	s := service.NewProductService(m)
+
+	p := domain.Product{
+		ID:          "unittest",
+		Name:        "Unittest",
+		SKU:         "UNIT",
+		Description: "Product for unittest",
+		Price:       10.0,
+		Available:   true,
+	}
+
+	err := s.UpdateProduct(&p)
+
+	if err != nil {
+		t.Error(err.Error())
+	}
 }
